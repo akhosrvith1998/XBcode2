031392784725f0fcda0d613d25340da7541b4046
akhosrvith1998 <a.khosrvith1898@gmail.com>
2025-05-28 00:09:20 +0330

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,8 +1,10 @@
 from alembic import context
 from sqlalchemy import engine_from_config, pool
-from models.whisper import AsyncBase
+from models.whisper import Base
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 config = context.config
 config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 connectable = engine_from_config(
@@ -14,7 +16,7 @@ connectable = engine_from_config(
 with connectable.connect() as connection:
     context.configure(
         connection=connection,
-        target_metadata=AsyncBase.metadata
+        target_metadata=Base.metadata
     )
-    with context.begin_transaction():
+    with context.begin():
         context.run_migrations()
