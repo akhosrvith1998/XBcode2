45439f8238c45e6d2320726917988138f7309500
akhosrvith1998 <a.khosrvith1898@gmail.com>
2025-05-27 22:53:48 +0330

    Create reply_middleware.py

---
+++ middleware/reply_middleware.py
@@ -0,0 +1,9 @@
+from aiogram import types
+from aiogram.dispatcher.middlewares import BaseMiddleware
+
+class ReplyMiddleware(BaseMiddleware):
+    async def on_process_message(self, message: types.Message, data: dict):
+        if message.reply_to_message:
+            data["target_id"] = message.reply_to_message.from_user.id
+        else:
+            data["target_id"] = None
