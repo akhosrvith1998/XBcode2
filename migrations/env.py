b7e6f60faaa75c57b20c3c07e2f9d642542621fb
akhosrvith1998 <a.khosrvith1898@gmail.com>
2025-05-28 00:35:37 +0330

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -7,16 +7,25 @@ from dotenv import load_dotenv
 load_dotenv()
 config = context.config
 config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
-connectable = engine_from_config(
-    config.get_section(config.config_ini_section),
-    prefix="sqlalchemy.",
-    poolclass=pool.NullPool,
-)
 
-with connectable.connect() as connection:
-    context.configure(
-        connection=connection,
-        target_metadata=Base.metadata
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
     )
-    with context.begin():
-        context.run_migrations()
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=Base.metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    # در حالت offline می‌توان اینجا تعریف کرد (در صورت نیاز)
+    pass
+else:
+    run_migrations_online()
