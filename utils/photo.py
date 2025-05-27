6e3a0fc42317137929230efb56cc1634e127e95a
akhosrvith1998 <a.khosrvith1898@gmail.com>
2025-05-27 22:54:45 +0330

    Create photo.py

---
+++ utils/photo.py
@@ -0,0 +1,12 @@
+from aiogram import Bot
+from aiocache import cached
+
+@cached(ttl=86400)
+async def get_user_profile_photo(user_id: int, bot: Bot):
+    try:
+        photos = await bot.get_user_profile_photos(user_id, limit=1)
+        if photos.photos:
+            return photos.photos[0][-1].file_id
+    except:
+        pass
+    return None
