bd3d9db4f33c910f95b33fa1a96b19597036b5ae
akhosrvith1998 <a.khosrvith1898@gmail.com>
2025-05-28 00:11:16 +0330

    Update photo.py

--- utils/photo.py
+++ utils/photo.py
@@ -1,12 +1,11 @@
 from aiogram import Bot
 from aiocache import cached
 
-@cached(ttl=86400)
-async def get_user_profile_photo(user_id: int, bot: Bot):
+@cached(ttl=3600)
+async def get_user_profile_photo(user_id: int, bot: Bot) -> str | None:
     try:
         photos = await bot.get_user_profile_photos(user_id, limit=1)
         if photos.photos:
             return photos.photos[0][-1].file_id
-    except:
-        pass
-    return None
+    except Exception:
+        return None
