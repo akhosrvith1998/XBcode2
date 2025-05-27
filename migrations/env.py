855b58052381dd2e6a2288696bf84d48021d9d7c
akhosrvith1998 <a.khosrvith1898@gmail.com>
2025-05-27 22:51:09 +0330

    Create env.py

---
+++ migrations/env.py
@@ -0,0 +1,20 @@
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+from models.whisper import Base
+import os
+
+config = context.config
+config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
+connectable = engine_from_config(
+    config.get_section(config.config_ini_section),
+    prefix="sqlalchemy.",
+    poolclass=pool.NullPool,
+)
+
+with connectable.connect() as connection:
+    context.configure(
+        connection=connection,
+        target_metadata=Base.metadata
+    )
+    with context.begin_transaction():
+        context.run_migrations()
