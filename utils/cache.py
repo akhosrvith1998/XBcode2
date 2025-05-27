d9481fd8a1b7b8887c6b9c3d1ed3a7bb4f79d08e
akhosrvith1998 <a.khosrvith1898@gmail.com>
2025-05-28 00:11:04 +0330

    Update cache.py

--- utils/cache.py
+++ utils/cache.py
@@ -3,11 +3,11 @@ from aiocache.decorators import cached
 
 cache = Cache(Cache.REDIS, endpoint="localhost", port=6379, namespace="whisper")
 
-@cached(ttl=3600, cache=Cache.REDIS)
-async def cached_get_user_id(username):
+@cached
+async def cached_get_user_id(username: str) -> int | None:
     from main import bot
     try:
         chat = await bot.get_chat(username)
         return chat.id
-    except:
+    except Exception:
         return None
